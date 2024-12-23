eb4ddbf8d102c6bb64abeb136426d3c8f380ca17
robiulsagor <robiul100.me@gmail.com>
2024-12-23 23:21:55 +0600

    user model created

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import connectDB from "./utils/db.js";
 
+dotenv.config();
 const app = express();
 
 // middleware
@@ -22,5 +24,6 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
